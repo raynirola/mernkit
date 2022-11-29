3fc7d353b80f12f04381c35d8c0688cbdd67a824
Ray Nirola <ray@nirola.tech>
2022-11-29 22:00:23 +0530

    upate server

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,21 @@ const config = {
   app: {
     secret: process.env.APP_KEY
   },
-  port: process.env.PORT || 3000
+  port: process.env.PORT
 }
+
+const validateConfigs = (config: string | object) => {
+  try {
+    Object.keys(config).forEach(key => {
+      if (typeof config[key] === 'object') validateConfigs(config[key])
+      if (!config[key]) throw new Error(`EnvironmentVariableError: \nMissing config value for ${key}`)
+    })
+  } catch (error) {
+    console.log(`\x1b[31m\x1b[1m${error.message}\x1b[0m`)
+    process.exit(1)
+  }
+}
+
+validateConfigs(config)
 
 export default config
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import config from '@/config'
 import * as routes from '@/routes'
-import * as middleware from '@/middlewares'
+import ErrorMiddleware from '@/middlewares/ErrorMiddleware'
 
 const app = express()
 
@@ -9,6 +9,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/', routes.home)
 app.use('/users', routes.user)
-app.use(middleware.error)
+app.use(ErrorMiddleware)
 
 app.listen(config.port, () => console.log(`Server is running at http://localhost:${config.port}`))
